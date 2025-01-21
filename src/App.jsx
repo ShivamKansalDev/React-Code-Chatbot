e86a3cf02aeb0e53021c32e45f20d65e63a0b94c
Ram Krishan <xeam.designer7>
2025-01-21 17:38:07 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import MainPage from "./MainPage"; // Your current main page component
 import ContactPage from "./ContactPage";
+import HomePage from "./Pages/HomePage/HomePage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
