c7cfe5a460be3f059010d1c94a896b5f9f5c564e
Ram Krishan <xeam.designer7>
2025-01-21 18:26:29 +0530

    home page done

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,14 +1,25 @@
 import { useNavigate } from "react-router-dom";
+import homepageLogo from "../../../src/assets/home_bottom_icons.png";
+import ChatModal from "../../Components/ChatModal/ChatModal";
+import { useState } from "react";
 
-const HomePage = () => {
-  const navigate = useNavigate();
+const HomePage = ({}) => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  // Open and close modal
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+
   return (
     <>
-      {/* <button className="bg-slate-950 py-24" onClick={() => navigate("/main")}>
-        HomePage
-      </button> */}
-      <div className="flex justify-center items-center h-screen bg-blue-500">
-        <h1 className="text-4xl text-white font-bold">Hello, Tailwind CSS!</h1>
+      <div className="absolute bottom-0 right-0">
+        <img
+          src={homepageLogo}
+          alt="image"
+          className="w-20 h-full cursor-pointer"
+          onClick={openModal} // Add this to open the modal
+        />
+        <ChatModal modalOpen={modalOpen} closeModal={closeModal} />
       </div>
     </>
   );
