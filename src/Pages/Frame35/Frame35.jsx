f3f61985250e0fc9ec916acb50b889b55f32de2c
apple <apple@MacBook-Pro.local>
2025-01-23 23:13:51 +0530

    frame done

---
+++ src/Pages/Frame35/Frame35.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import MessageBox from "../../Components/MessageBox";
+import star from "../../assets/images/star.png";
+import FaqComponent from "../../Components/FaqComponent";
+import rightArrow from "../../assets/images/rightArrow.png";
+import MessageUser from "../../Components/MessageUser";
+import user from "../../assets/images/user.png";
+import TabsCard from "../../Components/Tabs/TabsCard";
+
+const Frame35 = () => {
+  return (
+    <>
+      <div className="Frame35">
+        <div className="mb-5">
+          <FaqComponent
+            altText="dasds"
+            messageBody="Show similar products"
+            customStyles="mt-3"
+            imageSrc={rightArrow}
+            text="Budget Friendly"
+          />
+          <FaqComponent
+            altText="dasds"
+            messageBody="See lighter product options"
+            customStyles="mt-3"
+            imageSrc={rightArrow}
+            text="Expensive"
+          />
+          <FaqComponent
+            altText="dasds"
+            messageBody="See darker product options"
+            customStyles="mt-3"
+            imageSrc={rightArrow}
+            text="No Limit"
+          />
+        </div>
+        <div className="mb-5">
+          <MessageUser
+            imageSrc={user}
+            customStyles="mt-5"
+            messageBody="Know more about the product"
+          />
+        </div>
+        <div className="mb-5">
+          <MessageBox
+            imageSrc={star}
+            messageBody="Here are the detailed specifications for the Akadia Luxury Vinyl Flooring:"
+          />
+        </div>
+        <div className="pb-5">
+          <TabsCard />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Frame35;
--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -1,9 +1,27 @@
 import image1 from "../assets/images/floorthumb-1.png";
 import image2 from "../assets/images/floorthumb-2.png";
+import location from "../assets/images/location.png";
 
 export const CardData = [
   { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image1 },
   { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image2 },
+  { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image2 },
+  { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image1 },
+];
+
+export const Recommended_Options = [
+  { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image1 },
+  { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image2 },
+  { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image2 },
+  { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image1 },
+  { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image1 },
+  { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image2 },
+  { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image2 },
+  { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image1 },
+  { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image1 },
+  { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image2 },
+  { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image2 },
+  { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image1 },
 ];
 
 export const CardDataBottm = [
@@ -13,4 +31,37 @@ export const CardDataBottm = [
     bottomText: "I like this one",
     image: image1,
   },
+];
+
+export const CardWithLocation = [
+  {
+    title: "Katella Ash",
+    pre: "Luxury Vinyl Flooring",
+    bottomText: "I like this one",
+    image: location,
+  },
+  {
+    title: "Msi Alanata",
+    pre: "dasdnsadksalkdsaldsaj",
+    bottomText: "View website",
+    image: location, // Change to your image if needed
+  },
+  {
+    title: "Msi Alanata",
+    pre: "dasdnsadksalkdsaldsaj",
+    bottomText: "View website",
+    image: location, // Change to your image if needed
+  },
+  {
+    title: "Msi Alanata",
+    pre: "dasdnsadksalkdsaldsaj",
+    bottomText: "View website",
+    image: location, // Change to your image if needed
+  },
+];
+
+export const CardData2 = [
+  { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image1 },
+  { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image2 },
+  { title: "Katella Ash", pre: "Luxury Vinyl Flooring", image: image2 },
 ];
