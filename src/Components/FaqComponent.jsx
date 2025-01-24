d3b3f8d58600af3b9237a050d66f4dcdbc45e07f
xeamdesigner6 <xeam.designer6@gmail.com>
2025-01-24 14:52:11 +0530

    issue fixed

--- src/Components/FaqComponent.jsx
+++ src/Components/FaqComponent.jsx
@@ -12,13 +12,13 @@ function FaqComponent({
   return (
     <div className={`flex   rounded-[12px]  gap-[10px] ${customStyles}`}>
       <div
-        className={`flex justify-between items-center bg-white w-full  p-[12px] rounded-[100px] ${customBg}`}
+        className={`flex justify-between items-center bg-white w-full  py-[10px] px-[16px] rounded-[100px] ${customBg}`}
       >
         <div className="flex items-center justify-between gap-4">
           {messageTitle && <p className="font-bold">{messageTitle}</p>}
           <p>{messageBody}</p>
           {text && (
-            <p className="border border-[#755AEE] text-[#755AEE] px-[8px] py-[4px] rounded-full">
+            <p className="border border-[#755AEE] text-[#755AEE] px-[8px] py-[4px] text-[12px] rounded-full">
               {text}
             </p>
           )}
