cf693d3fcf8e5df798a0d3b48c737b147d0c9697
shivam.kansal202 <shivam.kansal202@gmail.com>
2025-01-24 13:08:57 +0530

    user

--- src/Pages/Frame32/Frame32.jsx
+++ src/Pages/Frame32/Frame32.jsx
@@ -1,25 +1,34 @@
 import UploadImage from "../../assets/images/house_upload.png";
 import imageFile from "../../assets/images/upload-image-file.png";
+import user from "../../assets/images/user.png";
+import MessageUser from "../../Components/MessageBox";
 
 export const Frame32 = () => {
   return (
-    <div className="bg-white px-3 py-3">
-      <h6 className="text-dark font-bold mb-5">
-        Upload your room photo for real time preview
-      </h6>
-      <div className="bg-[#F5F9FE] border-[#C0D8FB] rounded-lg flex flex-col justify-center items-center py-5 border-dotted border-2">
-        <img
-          src={UploadImage}
-          alt="Upload your room image"
-          className="w-16 h-auto"
-        />
-        <h6 className="font-semibold">Upload your room image</h6>
-        <p className="text-[12px] text-[#727681]">
-          Drop your image here, or
-          <span className="text-[#1D73F2] ms-2">browse</span>
-        </p>
+    <div className="px-3 py-3">
+      <div className="bg-white">
+        <h6 className="text-dark font-bold mb-5">
+          Upload your room photo for real time preview
+        </h6>
+        <div className="bg-[#F5F9FE] border-[#C0D8FB] rounded-lg flex flex-col justify-center items-center py-5 border-dotted border-2">
+          <img
+            src={UploadImage}
+            alt="Upload your room image"
+            className="w-16 h-auto"
+          />
+          <h6 className="font-semibold">Upload your room image</h6>
+          <p className="text-[12px] text-[#727681]">
+            Drop your image here, or
+            <span className="text-[#1D73F2] ms-2">browse</span>
+          </p>
+        </div>
       </div>
-      <div className="">
+      <div>
+        <img
+          src={user}
+          alt="user"
+          className="w-[30px] h-auto cursor-pointer absolute -right-2 mt-14"
+        />
         <img
           src={imageFile}
           alt="Upload your room image"
