d1523031ccadc005c566f0a420e68408b57cdb34
shivam.kansal202 <shivam.kansal202@gmail.com>
2025-01-24 12:32:08 +0530

    user

--- src/Components/MessageUser.jsx
+++ src/Components/MessageUser.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 
-function MessageUser({ imageSrc, altText , messageTitle, messageBody, customStyles = "" }) {
-
+function MessageUser({
+  imageSrc,
+  altText,
+  messageTitle,
+  messageBody,
+  customStyles = "",
+}) {
   return (
     <div className={`flex justify-end  gap-[10px] ${customStyles}`}>
-
-        
       <div className="bg-white p-[12px] rounded-[12px]">
-      {messageTitle && <p className="font-bold">{messageTitle}</p>}
+        {messageTitle && <p className="font-bold">{messageTitle}</p>}
         <p>{messageBody}</p>
       </div>
       {imageSrc ? (
-      <img
-        src={imageSrc}
-        alt={altText}
-        className="w-[26px] h-[26px] cursor-pointer"
-      />
-    ) : (
-      <div className="w-[26px] h-[26px]"></div> // Empty div placeholder
-    )}
+        <img
+          src={imageSrc}
+          alt={altText}
+          className="w-[26px] h-[26px] cursor-pointer absolute -right-5 mt-1"
+        />
+      ) : (
+        <div className="w-[26px] h-[26px]"></div> // Empty div placeholder
+      )}
     </div>
   );
 }
